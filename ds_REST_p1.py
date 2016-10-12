b1ec3c92b3a670c78183aa0219ed5d8946cf602a
김대호 <rlaeogh222@gmail.com>
2016-10-12 17:55:56 +0900

    Add files via upload

---
+++ ds_REST_p1.py
@@ -0,0 +1,22 @@
+﻿import os
+KEY="76526369436b6a6a37336445757871"
+TYPE = 'json'
+SERVICE= 'SearchSTNBySubwayLineService'
+START_INDEX='1'
+END_INDEX='10'
+LINE_NUM='2'
+url='http://openapi.seoul.go.kr:8088/'
+url+=KEY
+url+='/'
+url+=TYPE
+url+='/'
+url+=SERVICE
+url+='/'
+url+=START_INDEX
+url+='/'
+url+=END_INDEX
+url+='/'
+url+=LINE_NUM
+import requests
+data=requests.get(url)
+print data.text
